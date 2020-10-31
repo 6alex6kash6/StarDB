61f49544209c6a725f7d64348f5f596030731970
Alexandr <6alex6kash6@gmail.com>
2020-10-31 14:22:53 +0300

    feat: add routing for SPA

---
+++ src/components/pages/starships-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StarshipList } from '../sw-components';
+import { withRouter } from 'react-router-dom'
+
+export default function StarshipsPage({ history }) {
+  return (
+    <StarshipList onItemSelected={(itemId) => {
+      history.push(itemId)
+    }}/>
+  )
+}
